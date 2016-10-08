5066f91eb04faeedb775850b9c1de373efe8b24b
chertay <chertay@shaw.ca>
2016-10-08 21:03:24 +0000

    cleaned out testing items, placed tester into gitignore. Also, placed main.css in gitignore as the switch to style.php has worked

--- index.php
+++ index.php
@@ -16,11 +16,7 @@
       </div>
       <hr class="header">
       <div class="row text-center timenow">
-        <h4><?php
-        echo date('l M jS h:i A');
-        // echo "Today = " . $today;
-        // echo "<br>";
-        // echo "Time = " . $current_time;?></h4>
+        <h4><?php echo date('l M jS h:i A');?></h4>
       </div>
       <hr>
       <div class="row">
